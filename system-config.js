90e4064404f0880a49725816b3b0c15e0ffba0b8
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-14 15:40:53 -0700

    new gh-pages version

---
+++ system-config.js
@@ -0,0 +1,119 @@
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    '@angular2-material': 'vendor/@angular2-material',
+    'angular2-moment': 'vendor/angular2-moment',
+    'moment': 'vendor/moment'
+};
+/** User packages configuration. */
+var packages = {
+    '@angular2-material/core': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'core.js'
+    },
+    '@angular2-material/button': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'button.js'
+    },
+    '@angular2-material/card': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'card.js'
+    },
+    '@angular2-material/checkbox': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'checkbox.js'
+    },
+    '@angular2-material/icon': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'icon.js'
+    },
+    '@angular2-material/input': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'input.js'
+    },
+    '@angular2-material/list': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'list.js'
+    },
+    '@angular2-material/progress-bar': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'progress-bar.js'
+    },
+    '@angular2-material/progress-circle': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'progress-circle.js'
+    },
+    '@angular2-material/radio': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'radio.js'
+    },
+    '@angular2-material/sidenav': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'sidenav.js'
+    },
+    '@angular2-material/toolbar': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'toolbar.js'
+    },
+    'angular2-moment': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'index.js'
+    },
+    'moment': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'moment.js'
+    }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/+home',
+    'app/+settings',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
