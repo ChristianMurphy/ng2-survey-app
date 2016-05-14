a4de94b5494077464625a4db98817526ad658ea0
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-13 20:39:46 -0700

    setup material ui

--- src/app/ng2-survey-app.component.ts
+++ src/app/ng2-survey-app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MdButton } from '@angular2-material/button';
 
 @Component({
   moduleId: module.id,
   selector: 'ng2-survey-app-app',
   templateUrl: 'ng2-survey-app.component.html',
-  styleUrls: ['ng2-survey-app.component.css']
+  styleUrls: ['ng2-survey-app.component.css'],
+  directives: [MdButton]
 })
 export class Ng2SurveyAppAppComponent {
   title = 'ng2-survey-app works!';
