5046f797e6a531298204cb14e9d5bafabb9e0e3b
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-27 09:47:01 -0700

    bootstrap http provider

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { Ng2SurveyAppAppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(Ng2SurveyAppAppComponent);
+bootstrap(Ng2SurveyAppAppComponent, [
+    HTTP_PROVIDERS
+]);
 
