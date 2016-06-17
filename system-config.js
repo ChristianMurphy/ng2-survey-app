5630e5bc85ba9f0afd4992d306e9d9d9aa34d70e
Christian Murphy <Christian.Murphy.42@gmail.com>
2016-06-16 19:46:14 -0700

    new gh-pages version

--- system-config.js
+++ system-config.js
@@ -1,3 +1,6 @@
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
 /***********************************************************************************************
  * User Configuration.
  **********************************************************************************************/
@@ -5,80 +8,40 @@
 var map = {
     '@angular2-material': 'vendor/@angular2-material',
     'angular2-moment': 'vendor/angular2-moment',
-    'moment': 'vendor/moment'
+    'moment': 'vendor/moment',
+    'symbol-observable': 'vendor/symbol-observable'
 };
 /** User packages configuration. */
 var packages = {
-    // TODO: remove unused angular material packages
     '@angular2-material/core': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'core.js'
     },
     '@angular2-material/button': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'button.js'
     },
     '@angular2-material/card': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'card.js'
     },
     '@angular2-material/checkbox': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'checkbox.js'
     },
     '@angular2-material/icon': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'icon.js'
     },
     '@angular2-material/input': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'input.js'
     },
-    '@angular2-material/list': {
-        format: 'cjs',
-        defaultExtension: 'js',
-        main: 'list.js'
-    },
-    '@angular2-material/progress-bar': {
-        format: 'cjs',
-        defaultExtension: 'js',
-        main: 'progress-bar.js'
-    },
-    '@angular2-material/progress-circle': {
-        format: 'cjs',
-        defaultExtension: 'js',
-        main: 'progress-circle.js'
-    },
-    '@angular2-material/radio': {
-        format: 'cjs',
-        defaultExtension: 'js',
-        main: 'radio.js'
-    },
-    '@angular2-material/sidenav': {
-        format: 'cjs',
-        defaultExtension: 'js',
-        main: 'sidenav.js'
-    },
     '@angular2-material/toolbar': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'toolbar.js'
     },
     'angular2-moment': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'index.js'
     },
     'moment': {
-        format: 'cjs',
-        defaultExtension: 'js',
         main: 'moment.js'
+    },
+    'symbol-observable': {
+        main: 'index.js'
     }
 };
 ////////////////////////////////////////////////////////////////////////////////////////////////
